61ce65577b23b3af3738bfa55c234f93d06311b1
Phuriphat Petdee <cho.petdee@rockyou.com>
2019-06-10 15:26:28 +0700

    nuu

--- app.js
+++ app.js
@@ -84,7 +84,7 @@ passport.use(new GoogleStrategy({
                         }
                     }
                     console.log("Complete auth");
-                    return done(true);
+                    return done();
                 })
                 .catch(err => { //got error finding user, attemt to create new one
                     console.log(err);
