0ff8f61d17d6f7b5cc6681ef92d39e83e55f27f8
Phuriphat Petdee <cho.petdee@rockyou.com>
2019-06-21 15:24:44 +0700

    d

--- routes/products.js
+++ routes/products.js
@@ -275,7 +275,7 @@ router.post('/reset', (req, res) => {
         condotionP = { where: { [Op.or]: [{love: 1},{favorite:1}] }};
     }
     if(session[cookieToken].google_id){
-        Favorite.destroy(condotionF).then(() =>{}).catch(err => { res.send(err); });
+        Favorite.destroy(condotionF).then(() =>{}).catch(err => { return res.send(err); });
         Product.findAll(condotionP).then(products =>{
             for (var i = 0 ; i < products.length; i++) {
                 if (feeling == 'hard'){
@@ -285,7 +285,7 @@ router.post('/reset', (req, res) => {
                 products.love = 0;
             }
             products.save();
-            res.send(feeling);
+            return res.send(feeling);
         }).catch(err => { res.send(err); });
     }
 });
